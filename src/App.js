f704dfabe9f65dfebe09f35bd839dc254abb83fb
Josh Henderson <joshuahenderson031@gmail.com>
2023-12-29 11:23:03 -0700

    Working build

--- src/App.js
+++ src/App.js
@@ -1,25 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useQuery } from "@tanstack/react-query";
+import { Grid } from '@mui/material';
+import ImageList from '@mui/material/ImageList';
+import ImageListItem from '@mui/material/ImageListItem';
+import ImageListItemBar from '@mui/material/ImageListItemBar';
+import CircularProgress from '@mui/material/CircularProgress';
+import Alert from '@mui/material/Alert';
+import { SearchAppBar } from './SearchAppBar';
 
-function App() {
+
+
+
+const getPokemons = async () => {
+  const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=100");
+  const data = await response.json();
+
+  return data;
+};
+
+const getPokemon = async (url) => {
+  const response = await fetch(url);
+  const data = await response.json();
+
+  return data;
+};
+
+const PokemonTile = ({ name, url }) => {
+  const { error, isLoading, data } = useQuery({
+    queryKey: [`pokemon${name}`], 
+    queryFn: () => getPokemon(url)
+});
+
+  if (error) {
+    return <Alert severity="error">{error}</Alert>;
+  }
+
+  if (isLoading) {
+    return (
+      <Grid container justify="center">
+        <CircularProgress />
+      </Grid>
+    );
+  }
+
+  const {
+    sprites: { front_default }
+  } = data;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ImageListItem>
+      <img src={front_default} alt={name} />
+      <ImageListItemBar title={name} />
+    </ImageListItem>
   );
-}
+};
 
-export default App;
+export const App = () => {
+  const { error, isLoading, data } = useQuery({
+    queryKey: ["pokemons"],
+    queryFn: getPokemons});
+
+  if (error) {
+    return <Alert severity="error">{error}</Alert>;
+  }
+
+  if (isLoading) {
+    return (
+      <Grid container justify="center">
+        <CircularProgress />
+      </Grid>
+    );
+  }
+
+  const { results: pokemons } = data;
+
+  return (
+    <ImageList cellHeight={300}>
+      {pokemons.map((pokemon) => (
+        <PokemonTile key={pokemon.name} {...pokemon} />
+      ))}
+    </ImageList>
+  );
+};
--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { StrictMode } from "react";
+import { App } from "./App";
+import { createRoot } from 'react-dom/client';
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+const container = document.getElementById('root');
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const queryClient = new QueryClient();
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
+  </StrictMode>,
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
